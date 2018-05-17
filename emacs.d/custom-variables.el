8b9cd1677ceb758c7e3ebf84e0bc9c4d811c8670
Andrey Babushkin <andrey.babushkin@intel.com>
2018-05-17 20:16:54 +0300

    [emacs.d/custom-variables.el] Add selected packages

--- emacs.d/custom-variables.el
+++ emacs.d/custom-variables.el
@@ -9,7 +9,10 @@
  '(custom-safe-themes
    (quote
     ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
- '(indent-tabs-mode nil))
+ '(indent-tabs-mode nil)
+ '(package-selected-packages
+   (quote
+    (flycheck groovy-mode yaml-mode ws-butler web-mode use-package smex smart-mode-line neotree magit dockerfile-mode diminish ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
