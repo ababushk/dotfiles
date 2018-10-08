b2c67b7c6b8b03b9d3ed6b2ca9abae3a72ab782b
Andrey Babushkin <oxygenxo@gmail.com>
2018-10-08 22:54:24 +0300

    More custom variables

--- emacs.d/custom-variables.el
+++ emacs.d/custom-variables.el
@@ -6,15 +6,47 @@
  '(after-save-hook
    (quote
     (executable-make-buffer-file-executable-if-script-p)))
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
  '(aw-dispatch-always t)
  '(comment-multi-line t)
+ '(custom-enabled-themes (quote (dracula)))
  '(custom-safe-themes
    (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
+    ("3a3de615f80a0e8706208f0a71bbcc7cc3816988f971b6d237223b6731f91605" "f0dc4ddca147f3c7b1c7397141b888562a48d9888f1595d69572db73be99a024" "a3fa4abaf08cc169b61dea8f6df1bbe4123ec1d2afeb01c17e11fdc31fc66379" "36ca8f60565af20ef4f30783aa16a26d96c02df7b4e54e9900a5138fb33808da" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "aaffceb9b0f539b6ad6becb8e96a04f2140c8faa1de8039a343a4f1e009174fb" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
+ '(fci-rule-color "#555556")
+ '(hl-sexp-background-color "#121212")
  '(indent-tabs-mode nil)
+ '(jdee-db-active-breakpoint-face-colors (cons "#000000" "#fd971f"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#000000" "#b6e63e"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#000000" "#525254"))
+ '(org-babel-load-languages (quote ((emacs-lisp . t) (shell . t) (python . t))))
+ '(org-confirm-babel-evaluate nil)
  '(package-selected-packages
    (quote
-    (yasnippet flycheck groovy-mode yaml-mode ws-butler web-mode use-package smex smart-mode-line neotree magit dockerfile-mode diminish ace-window))))
+    (rainbow-delimiters doom-themes ample-theme material-theme dracula-theme org-jira markdown-mode org-plus-contrib yasnippet flycheck groovy-mode yaml-mode ws-butler web-mode use-package smex smart-mode-line neotree magit dockerfile-mode diminish ace-window)))
+ '(vc-annotate-background "#1c1e1f")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#b6e63e")
+    (cons 40 "#c4db4e")
+    (cons 60 "#d3d15f")
+    (cons 80 "#e2c770")
+    (cons 100 "#ebb755")
+    (cons 120 "#f3a73a")
+    (cons 140 "#fd971f")
+    (cons 160 "#fc723b")
+    (cons 180 "#fb4d57")
+    (cons 200 "#fb2874")
+    (cons 220 "#f43461")
+    (cons 240 "#ed404e")
+    (cons 260 "#e74c3c")
+    (cons 280 "#c14d41")
+    (cons 300 "#9c4f48")
+    (cons 320 "#77504e")
+    (cons 340 "#555556")
+    (cons 360 "#555556")))
+ '(vc-annotate-very-old-color nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
