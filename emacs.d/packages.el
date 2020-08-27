8e4ab9d22c368466bce8381b3aeab9921ad5bbbd
Andrey Babushkin <andrey.babushkin@intel.com>
2020-08-27 16:26:31 +0300

    [emacs/packages] Fix themes as I set true color in terminal

--- emacs.d/packages.el
+++ emacs.d/packages.el
@@ -70,20 +70,16 @@
     (setq web-mode-enable-auto-expanding t)
     (setq web-mode-enable-css-colorization t)))
 
-;; Commented because I'm working from home via SSH on my Linux machine T_T
-;; (use-package doom-themes
-;;   ;; :pin melpa ;; horizon theme available in newer versions only
-;;   :config
-;;   ;; Global settings (defaults)
-;;   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-;;         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-;;   (load-theme 'doom-dracula t)
+(use-package doom-themes
+  :pin melpa ;; horizon theme available in newer versions only
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-gruvbox t)
 
-;;   ;; Enable flashing mode-line on errors
-;;   (doom-themes-visual-bell-config)
-
-;;   ;; Corrects (and improves) org-mode's native fontification.
-;;   (doom-themes-org-config))
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config))
 
 (use-package rainbow-delimiters
   :ensure t
